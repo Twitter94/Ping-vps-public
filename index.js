ce5e33ce81b8a61c32d9074b9efa8b1c71e3d7bb
Twitter94 <157843216+Twitter94@users.noreply.github.com>
2026-06-29 05:43:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-const ccxt = require('ccxt'); // Hapus aja dulu kalau belum dipake
 
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
